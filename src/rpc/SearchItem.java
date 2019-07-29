aebb9ff98f19485f899082255f87b1a38b6612ac
Fangyi Zhao <zoefyz12@gmail.com>
2019-07-28 23:29:02 -0700

    First commit

---
+++ src/rpc/SearchItem.java
@@ -0,0 +1,106 @@
+package rpc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import entity.Item;
+import external.TicketMasterClient;
+
+/**
+ * Servlet implementation class SearchItem
+ */
+@WebServlet("/search")
+public class SearchItem extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchItem() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		/*Step 6*/
+//		response.setContentType("text/html");
+//		PrintWriter writer = response.getWriter();
+//		
+//		writer.println("<html><body>");
+//		writer.println("<h1>Hello World</h1>");
+//		writer.println("</body></html>");
+//		
+//		writer.close();		
+		
+		
+		/*Step 8*/
+//		response.setContentType("text/html");
+//		PrintWriter writer = response.getWriter();
+//		
+//		if (request.getParameter("username") != null) {
+//			String username = request.getParameter("username");
+//			
+//			writer.println("<html><body>");
+//			writer.println("<h1>Hello " + username + "</h1>");
+//			writer.println("</body></html>");
+//		}
+//		
+//		writer.close();	
+//		
+//		response.setContentType("application/json");
+//        PrintWriter writer = response.getWriter();
+//		
+//		JSONArray array = new JSONArray();
+//		try {
+//			array.put(new JSONObject().put("username", "abcd"));
+//			array.put(new JSONObject().put("username", "1234"));
+//		} catch (JSONException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		writer.print(array);
+//		writer.close();
+		
+		double lat = Double.parseDouble(request.getParameter("lat"));
+		double lon = Double.parseDouble(request.getParameter("lon"));
+
+		TicketMasterClient client = new TicketMasterClient();
+		List<Item> items = client.search(lat, lon, null);
+		JSONArray array = new JSONArray();
+		for (Item item : items) {
+			array.put(item.toJSONObject());
+		}
+		RpcHelper.writeJsonArray(response, array);
+
+	
+	}
+
+
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		doGet(request, response);
+//	}
+
+}
